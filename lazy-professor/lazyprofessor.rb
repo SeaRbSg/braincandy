85e8fcb7cb4191f9db57112cb8da35842c36f12e
Ryan Davis <ryand-ruby@zenspider.com>
2015-08-14 15:49:18 -0700

    Fix indentation

--- lazy-professor/lazyprofessor.rb
+++ lazy-professor/lazyprofessor.rb
@@ -1,18 +1,18 @@
 students = File.read "test.txt"
 
 def best_score(students = "")
-    scores = students.gsub(/\s+/m, ' ').strip.split
-    test_score_counter = students.split[0]
-    grade_counter = test_score_counter.split('').count
+  scores = students.gsub(/\s+/m, ' ').strip.split
+  test_score_counter = students.split[0]
+  grade_counter = test_score_counter.split('').count
 
-    grade_counter.times.map do |counter|
-      current_scores = scores.map {|w| w[counter] }
-      count = Hash.new 0
-      current_scores.each do |grade|
-        count[grade] += 1
-      end
-      count.keys.max_by { |grade| count[grade] }
+  grade_counter.times.map do |counter|
+    current_scores = scores.map {|w| w[counter] }
+    count = Hash.new 0
+    current_scores.each do |grade|
+      count[grade] += 1
     end
+    count.keys.max_by { |grade| count[grade] }
+  end
 end
 
 puts best_score(students).join
