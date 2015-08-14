9ee01995e4eb0c5181743c9dbbf4afcac077477b
Ryan Davis <ryand-ruby@zenspider.com>
2015-08-14 15:42:30 -0700

    redundant to_i

--- lazy-professor/lazyprofessor.rb
+++ lazy-professor/lazyprofessor.rb
@@ -3,7 +3,7 @@ students = File.read "test.txt"
 def best_score(students = "")
     scores = students.gsub(/\s+/m, ' ').strip.split
     test_score_counter = students.split[0]
-    grade_counter = test_score_counter.split('').count.to_i
+    grade_counter = test_score_counter.split('').count
 
     grade_counter.times do |counter|
       puts scores.map {|w| w[counter]}.each_with_object(Hash.new(0)).max_by { |grade, count| count[grade] += 1}
