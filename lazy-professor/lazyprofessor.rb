e6c75cf0614026d5f85a3b3c5417183a791e579c
Ryan Davis <ryand-ruby@zenspider.com>
2015-08-14 15:41:51 -0700

    refactor until+manual counting into a proper iterator

--- lazy-professor/lazyprofessor.rb
+++ lazy-professor/lazyprofessor.rb
@@ -4,11 +4,9 @@ def best_score(students = "")
     scores = students.gsub(/\s+/m, ' ').strip.split
     test_score_counter = students.split[0]
     grade_counter = test_score_counter.split('').count.to_i
-    counter = 0
 
-    until counter > grade_counter do
+    grade_counter.times do |counter|
       puts scores.map {|w| w[counter]}.each_with_object(Hash.new(0)).max_by { |grade, count| count[grade] += 1}
-      counter += 1
     end
 
 end
