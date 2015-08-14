3cf66557ec09ae989cb4db3021b44736382de68f
Ryan Davis <ryand-ruby@zenspider.com>
2015-08-14 15:48:52 -0700

    Switch from each_with_object and an inner print to a map and outer puts.

--- lazy-professor/lazyprofessor.rb
+++ lazy-professor/lazyprofessor.rb
@@ -5,10 +5,14 @@ def best_score(students = "")
     test_score_counter = students.split[0]
     grade_counter = test_score_counter.split('').count
 
-    grade_counter.times do |counter|
-      puts scores.map {|w| w[counter]}.each_with_object(Hash.new(0)).max_by { |grade, count| count[grade] += 1}
+    grade_counter.times.map do |counter|
+      current_scores = scores.map {|w| w[counter] }
+      count = Hash.new 0
+      current_scores.each do |grade|
+        count[grade] += 1
+      end
+      count.keys.max_by { |grade| count[grade] }
     end
-
 end
 
-best_score(students)
+puts best_score(students).join
